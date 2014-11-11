e70c6ff45ce5c89481595222afd994297182d8ed
Noah Lemen <noah.lemen@gmail.com>
2014-11-11 14:55:18 -0500

    Initial Commit

---
+++ basketclimb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  basketclimb
+//
+//  Created by Noah Lemen on 11/11/14.
+//  Copyright (c) 2014 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
