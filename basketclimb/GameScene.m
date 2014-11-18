9f115c9e31ff226687425c31f88f11bc4a8b373a
Noah Lemen <noah.lemen@gmail.com>
2014-11-18 16:55:41 -0500

    indicator offset

--- basketclimb/GameScene.m
+++ basketclimb/GameScene.m
@@ -8,6 +8,7 @@
 
 #import "GameScene.h"
 const float FORCE_MULT = .2;
+const float MIN_INPUT = 40.0;
 
 @implementation GameScene{
     CGPoint touchBegan;
@@ -59,37 +60,48 @@ const float FORCE_MULT = .2;
     [touchline removeFromParent];
     [touchline2 removeFromParent];
     
-    CGMutablePathRef pathToDraw = CGPathCreateMutable();
-    CGPathMoveToPoint(pathToDraw, NULL, touchBegan.x, touchBegan.y);
-    CGPathAddLineToPoint(pathToDraw, NULL, 2*touchBegan.x-touchPoint.x, 2*touchBegan.y-touchPoint.y);
-    CGPathCloseSubpath(pathToDraw);
-    
-    CGMutablePathRef pathToDraw2 = CGPathCreateMutable();
-    CGPathMoveToPoint(pathToDraw2, NULL, touchBegan.x, touchBegan.y);
-    CGPathAddLineToPoint(pathToDraw2, NULL, touchPoint.x, touchPoint.y);
-    CGPathCloseSubpath(pathToDraw2);
-    
-    touchline = [SKShapeNode node];
-    touchline.lineWidth = 3;
-    touchline.path = pathToDraw;
-    CGPathRelease(pathToDraw);
-    [touchline setStrokeColor:[UIColor whiteColor]];
-    [self addChild:touchline];
-    
-    touchline2 = [SKShapeNode node];
-    touchline2.path = pathToDraw2;
-    CGPathRelease(pathToDraw2);
-    [touchline2 setStrokeColor:[UIColor colorWithWhite:1 alpha:.5]];
-    [self addChild:touchline2];
-    
+    if ([self distanceFrom:touchBegan to:touchPoint] > MIN_INPUT){
+        
+        GLKVector2 direction = GLKVector2Normalize(GLKVector2Make(touchPoint.x - touchBegan.x, touchPoint.y - touchBegan.y));
+        GLKVector2 frontLineBegin =  GLKVector2Subtract(GLKVector2Make(touchBegan.x, touchBegan.y), GLKVector2MultiplyScalar(direction, MIN_INPUT));
+        GLKVector2 backLineBegin = GLKVector2Add(GLKVector2Make(touchBegan.x, touchBegan.y), GLKVector2MultiplyScalar(direction, MIN_INPUT));
+        
+        CGMutablePathRef pathToDraw = CGPathCreateMutable();
+        CGPathMoveToPoint(pathToDraw, NULL, frontLineBegin.x, frontLineBegin.y);
+        CGPathAddLineToPoint(pathToDraw, NULL, 2*touchBegan.x-touchPoint.x, 2*touchBegan.y-touchPoint.y);
+        CGPathCloseSubpath(pathToDraw);
+        
+        CGMutablePathRef pathToDraw2 = CGPathCreateMutable();
+        CGPathMoveToPoint(pathToDraw2, NULL, backLineBegin.x, backLineBegin.y);
+        CGPathAddLineToPoint(pathToDraw2, NULL, touchPoint.x, touchPoint.y);
+        CGPathCloseSubpath(pathToDraw2);
+        
+        touchline = [SKShapeNode node];
+        touchline.lineWidth = 3;
+        touchline.path = pathToDraw;
+        CGPathRelease(pathToDraw);
+        [touchline setStrokeColor:[UIColor whiteColor]];
+        [self addChild:touchline];
+        
+        touchline2 = [SKShapeNode node];
+        touchline2.path = pathToDraw2;
+        CGPathRelease(pathToDraw2);
+        [touchline2 setStrokeColor:[UIColor colorWithWhite:1 alpha:.5]];
+        [self addChild:touchline2];
+    }
 }
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
+    
     [touchline removeFromParent];
     [touchline2 removeFromParent];
+    
     UITouch *touch = [touches anyObject];
     touchEnd = [touch locationInNode:self];
-    [[self childNodeWithName:@"ball"].physicsBody applyForce:CGVectorMake((touchBegan.x-touchEnd.x)*FORCE_MULT, (touchBegan.y-touchEnd.y)*FORCE_MULT)];
+    
+    if ([self distanceFrom:touchBegan to:touchEnd] > MIN_INPUT){
+        [[self childNodeWithName:@"ball"].physicsBody applyForce:CGVectorMake((touchBegan.x-touchEnd.x)*FORCE_MULT, (touchBegan.y-touchEnd.y)*FORCE_MULT)];
+    }
     
 }
 
@@ -97,5 +109,12 @@ const float FORCE_MULT = .2;
     /* Called before each frame is rendered */
     
 }
+
+-(float)distanceFrom:(CGPoint)from to:(CGPoint)to{
+    float dx = to.x - from.x;
+    float dy = to.y - from.y;
+    return sqrtf(dx*dx + dy*dy);
+}
+
 
 @end
