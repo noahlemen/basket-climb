be0575ff42b7170dbafdbb6a1123adaa5392ad05
Noah Lemen <noah.lemen@gmail.com>
2014-11-18 12:09:00 -0500

    tuning, only allow throw when ball is resting

--- basketclimb/GameScene.m
+++ basketclimb/GameScene.m
@@ -7,7 +7,7 @@
 //
 
 #import "GameScene.h"
-const int FORCE_MULT = 20;
+const float FORCE_MULT = .2;
 
 @implementation GameScene{
     CGPoint touchBegan;
@@ -25,17 +25,18 @@ const int FORCE_MULT = 20;
     
     SKPhysicsBody* borderBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     self.physicsBody = borderBody;
-    self.physicsBody.friction = 1.0f;
+    self.physicsBody.friction = 0.5f;
     
     SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
-    ball.xScale = .5;
-    ball.yScale = .5;
+    ball.xScale = .25;
+    ball.yScale = .25;
     ball.name = @"ball";
     ball.position = CGPointMake(CGRectGetMidX(self.frame),
                                 CGRectGetMidY(self.frame));
     [self addChild:ball];
     
     ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+    ball.physicsBody.allowsRotation = NO;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -54,39 +55,45 @@ const int FORCE_MULT = 20;
     // offset will need to be circular, remember to use RMS if needed - also subtract that offset from the length of the indicator?
     // ideally if touching at or less than minimal magnitude, no lines are shown
     
-    [touchline removeFromParent];
-    [touchline2 removeFromParent];
+    if ([self childNodeWithName:@"ball"].physicsBody.resting){
+        [touchline removeFromParent];
+        [touchline2 removeFromParent];
+        
+        CGMutablePathRef pathToDraw = CGPathCreateMutable();
+        CGPathMoveToPoint(pathToDraw, NULL, touchBegan.x, touchBegan.y);
+        CGPathAddLineToPoint(pathToDraw, NULL, 2*touchBegan.x-touchPoint.x, 2*touchBegan.y-touchPoint.y);
+        CGPathCloseSubpath(pathToDraw);
+        
+        CGMutablePathRef pathToDraw2 = CGPathCreateMutable();
+        CGPathMoveToPoint(pathToDraw2, NULL, touchBegan.x, touchBegan.y);
+        CGPathAddLineToPoint(pathToDraw2, NULL, touchPoint.x, touchPoint.y);
+        CGPathCloseSubpath(pathToDraw2);
+        
+        touchline = [SKShapeNode node];
+        touchline.lineWidth = 3;
+        touchline.path = pathToDraw;
+        CGPathRelease(pathToDraw);
+        [touchline setStrokeColor:[UIColor whiteColor]];
+        [self addChild:touchline];
+        
+        touchline2 = [SKShapeNode node];
+        touchline2.path = pathToDraw2;
+        CGPathRelease(pathToDraw2);
+        [touchline2 setStrokeColor:[UIColor colorWithWhite:1 alpha:.5]];
+        [self addChild:touchline2];
+    }
     
-    CGMutablePathRef pathToDraw = CGPathCreateMutable();
-    CGPathMoveToPoint(pathToDraw, NULL, touchBegan.x, touchBegan.y);
-    CGPathAddLineToPoint(pathToDraw, NULL, 2*touchBegan.x-touchPoint.x, 2*touchBegan.y-touchPoint.y);
-    CGPathCloseSubpath(pathToDraw);
-    
-    CGMutablePathRef pathToDraw2 = CGPathCreateMutable();
-    CGPathMoveToPoint(pathToDraw2, NULL, touchBegan.x, touchBegan.y);
-    CGPathAddLineToPoint(pathToDraw2, NULL, touchPoint.x, touchPoint.y);
-    CGPathCloseSubpath(pathToDraw2);
-    
-    touchline = [SKShapeNode node];
-    touchline.lineWidth = 1.5;
-    touchline.path = pathToDraw;
-    CGPathRelease(pathToDraw);
-    [touchline setStrokeColor:[UIColor whiteColor]];
-    [self addChild:touchline];
-    
-    touchline2 = [SKShapeNode node];
-    touchline2.path = pathToDraw2;
-    CGPathRelease(pathToDraw2);
-    [touchline2 setStrokeColor:[UIColor colorWithWhite:1 alpha:.2]];
-    [self addChild:touchline2];
 }
 
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
-    [touchline removeFromParent];
-    [touchline2 removeFromParent];
-    UITouch *touch = [touches anyObject];
-    touchEnd = [touch locationInNode:self];
-    [[self childNodeWithName:@"ball"].physicsBody applyForce:CGVectorMake((touchBegan.x-touchEnd.x)*FORCE_MULT, (touchBegan.y-touchEnd.y)*FORCE_MULT)];
+    if ([self childNodeWithName:@"ball"].physicsBody.resting){
+        [touchline removeFromParent];
+        [touchline2 removeFromParent];
+        UITouch *touch = [touches anyObject];
+        touchEnd = [touch locationInNode:self];
+        [[self childNodeWithName:@"ball"].physicsBody applyForce:CGVectorMake((touchBegan.x-touchEnd.x)*FORCE_MULT, (touchBegan.y-touchEnd.y)*FORCE_MULT)];
+    }
+    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
